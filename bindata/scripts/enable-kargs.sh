ee1c2ebf33b884e70b2be307f8c8b2a743aecc3d
Federico Paolinelli <fpaoline@redhat.com>
2019-08-01 16:00:50 +0200

    Assume kernel args are setup correctly if grubby is not on board

--- bindata/scripts/enable-kargs.sh
+++ bindata/scripts/enable-kargs.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -x
+
+which grubby
+
+# if grubby is not there, assume everything is fine and the setup was done on the host
+if [ $? -ne 0 ]; then
+    echo 0
+fi
+
 declare -a kargs=( "$@" )
 eval `chroot /host/ grubby --info=DEFAULT | grep args`
 ret=0
